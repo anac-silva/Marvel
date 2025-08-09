d7eb7e8d8ffee2c5db30143f347ac946fddd516a
Ana Carolina Silva <ana.carolina3551@gmail.com>
2025-08-09 16:48:10 -0300

    Feat(import): corrige caminho no arquivo main

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
 import App from "./App.jsx";
 import { ComicsProvider } from "./context/ComicsContext";
 import { CartProvider } from "./context/CartContext";
